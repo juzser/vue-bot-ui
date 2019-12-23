ebf2e728a503ae3e0c56d7cac527afd83552b864
Doan Duy Dat <doanduydat98@gmail.com>
2019-12-23 12:13:07 +0700

    Add: auto scroll to bottom

--- src/components/board/Content.vue
+++ src/components/board/Content.vue
@@ -1,20 +1,25 @@
 <template lang="pug">
-  .qkb-board-content
+  #bcontent.qkb-board-content(
+    ref="boardContent"
+  )
     template(v-if='listMessageBubble.length')
       message-bubble(
         v-for='(message, i) in listMessageBubble',
         :key='i',
         :message='message'
       )
-
 </template>
 
 <script>
 import MessageBubble from './MessageBubble'
+import ScrollBar from './ScrollBar'
+
 import { mapState } from 'vuex'
+
 export default {
   components: {
-    MessageBubble
+    MessageBubble,
+    ScrollBar
   },
   data () {
     return {
@@ -24,6 +29,20 @@ export default {
     ...mapState([
       'listMessageBubble'
     ])
+  },
+  watch: {
+    listMessageBubble: function (newVal, oldVal) {
+      this.$nextTick(() => {
+        this.updateScroll()
+      })
+    }
+  },
+  methods: {
+    updateScroll () {
+      const boardContent = this.$refs.boardContent
+      const currentHeight = boardContent.scrollHeight
+      this.$refs.boardContent.scrollBy(0, currentHeight)
+    }
   }
 }
 </script>
