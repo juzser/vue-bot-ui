95b70fd39a534e54694ff26e489998c31ef9c79b
Doan Duy Dat <doanduydat98@gmail.com>
2019-12-23 10:43:44 +0700

    Add: fake-message

---
+++ src/_helper/fake-message.js
@@ -0,0 +1,68 @@
+export const fakeMessage = [
+  {
+    'type': 'text',
+    'text': 'Hello. Have a nice day!',
+    'disableInput': true
+  },
+  {
+    'type': 'button',
+    'text': 'How can we help you today?',
+    'data': [
+      {
+        'title': 'Search Suport Articles',
+        'value': 'search',
+        'action': 'postback'
+      },
+      {
+        'title': 'Submit Support Ticket',
+        'value': 'submit_ticket',
+        'action': 'postback'
+      }
+    ],
+    'disableInput': false
+  },
+  {
+    'type': 'text',
+    'text': 'Please type your problem',
+    'disableInput': false
+  },
+  {
+    'type': 'button',
+    'text': 'Here are the results from our knowledgebase.',
+    'data': [
+      {
+        'title': 'How to create a menu',
+        'value': 'https://google.com',
+        'action': 'url'
+      },
+      {
+        'title': 'How to add a submenu to a menu',
+        'value': 'https://google.com',
+        'action': 'url'
+      },
+      {
+        'title': "Not what I'm looking for",
+        'value': 'result_not_match',
+        'action': 'postback'
+      }
+    ],
+    'disableInput': true
+  },
+  {
+    'type': 'text',
+    'text': 'Sorry to hear that. Please type your problem and we will create a ticket for you.',
+    'disableInput': false
+  },
+  {
+    'type': 'button',
+    'text': 'All done! Your support ticket is created.',
+    'data': [
+      {
+        'title': 'View ticket',
+        'value': 'https://google.com',
+        'action': 'url'
+      }
+    ],
+    'disableInput': true
+  }
+]
