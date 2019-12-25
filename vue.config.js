a28fc9e32e94ace001a65bcdd17c3bd0a7173013
oe.sonnh <sonnh.90@gmail.com>
2019-12-25 14:31:08 +0700

    Add: Svg loader to vue config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,17 @@ module.exports = {
   devServer: {
     port: 1901
   },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg')
+
+    svgRule.uses.clear()
+
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+  },
   productionSourceMap: false
 }
