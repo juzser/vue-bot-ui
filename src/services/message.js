f3266eab98b518b633f2d731b492f08649e76aa1
oe.sonnh <sonnh.90@gmail.com>
2019-12-25 17:55:15 +0700

    Add helpers services directory

---
+++ src/services/message.js
@@ -0,0 +1,36 @@
+import { fakeMessage } from '../helpers/fake-message'
+
+export const messageService = {
+  getAllMessages,
+  createMessage,
+  getMessage
+}
+
+function getAllMessages () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let randomNumber = Math.floor(Math.random() * fakeMessage.length)
+      console.log(randomNumber)
+      resolve(fakeMessage[randomNumber])
+    }, 1000)
+  })
+}
+
+function createMessage (messageText) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let randomNumber = Math.floor(Math.random() * fakeMessage.length)
+      resolve(fakeMessage[randomNumber])
+    }, 1000)
+  })
+}
+
+function getMessage () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let randomNumber = Math.floor(Math.random() * fakeMessage.length)
+      console.log(randomNumber)
+      resolve(fakeMessage[randomNumber])
+    }, 1000)
+  })
+}
