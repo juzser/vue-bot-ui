6e7233289ef7b298d0bbc59ac2376a1caec79a22
oe.sonnh <sonnh.90@gmail.com>
2019-12-31 11:59:48 +0700

    AppStyle for dynamic styles

---
+++ src/components/AppStyle.vue
@@ -0,0 +1,75 @@
+<template lang="pug">
+.qkb-bot-style(v-html="style", style="display: none")
+</template>
+<script>
+export default {
+  props: ['options'],
+  computed: {
+    style () {
+      if (!this.options) {
+        return ''
+      }
+
+      const {
+        colorScheme,
+        textColor,
+        bubbleBtnSize,
+        botAvatarImg,
+        botAvatarSize,
+        msgBubbleBgBot,
+        msgBubbleColorBot,
+        msgBubbleBgUser,
+        msgBubbleColorUser
+      } = this.options
+
+      const styles = `
+<style type="text/css" id="qkb-bot-style">
+.qkb-bubble-btn {
+  background-color: ${colorScheme};
+  width: ${bubbleBtnSize}px;
+  height: ${bubbleBtnSize}px;
+}
+.qkb-bubble-btn-icon {
+  fill: ${textColor};
+  color: ${textColor};
+}
+.qkb-board {
+  bottom: ${bubbleBtnSize}px;
+}
+.qkb-board-header {
+  background-color: ${colorScheme};
+}
+.qkb-board-header__title {
+  color: ${textColor};
+}
+${botAvatarImg
+    ? `.qkb-msg-avatar {
+      width: ${botAvatarSize}px;
+      height: ${botAvatarSize}px;
+    }
+    .qkb-msg-avatar__img {
+      background-image: url(${botAvatarImg});
+    }`
+    : ''
+}
+.qkb-msg-bubble--bot .qkb-msg-bubble-component__text {
+  color: ${msgBubbleColorBot};
+  background-color: ${msgBubbleBgBot};
+}
+.qkb-mb-button-options__btn::before {
+  background-color: ${msgBubbleBgUser};
+}
+.qkb-msg-bubble--user .qkb-msg-bubble-component__text {
+  color: ${msgBubbleColorUser};
+  background-color: ${msgBubbleBgUser};
+}
+.qkb-mb-button-options__btn {
+  color: ${msgBubbleColorUser};
+}
+</style>
+      `
+      return styles
+    }
+  }
+}
+</script>
