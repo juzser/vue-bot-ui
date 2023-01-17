16804b62c2085d74830dcd90ff3496402618aeb9
oe.sonnh <sonnh@ownego.com>
2023-01-17 11:21:54 +0700

    Clean the repo

--- src/components/MessageBubble/Typing.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-.qkb-msg-bubble.qkb-msg-bubble--bot
-  .qkb-msg-avatar
-    .qkb-msg-avatar__img &nbsp;
-  .qkb-msg-bubble-component.qkb-msg-bubble-component--typing
-    .qkb-msg-bubble-component__text
-      .qkb-msg-bubble__typing-indicator
-        span
-</template>
--- src/config/index.js
+++
@@ -1,5 +0,0 @@
-export default {
-  EVENT_OPEN: 'botui-open',
-  EVENT_CLOSE: 'botui-close',
-  EVENT_TOGGLE: 'botui-toggle'
-}
